9013c7ab66555f08b403e718bc5b37975d0f8764
huci <huci@dianjia.io>
2018-05-03 22:09:55 +0800

    update backend

--- server/app.js
+++ server/app.js
@@ -13,6 +13,33 @@ const Cat = mongoose.model('Cat', { name: String });
 const kitty = new Cat({ name: 'Zildjian' });
 kitty.save().then(() => console.log('meow'));
 
+
+
+const Work = mongoose.model('Work', {
+    title: String,
+    info: String,
+    customerId: String,
+    followUpDate: String,
+    followUp: Array,
+})
+
+const FollowUp = mongoose.model('followUp', {
+  customerId: String,
+  info: String,
+  followUpDate: String,
+})
+
+const f1 = new FollowUp({customerId:'1111', info: 'fllowuppup', followUpDate: 'fff'});
+const work1 = new Work({
+  title: 'fengjin',
+  info: 'infoaaaa',
+  customerId: '1111',
+  followUpDate: '2fff',
+  followUp: [f1]
+})
+
+work1.save().then(() => console.log('work'));
+
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
 });
